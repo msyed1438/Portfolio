8b95b25641df90cc505cf6a7e4bd42d40897c554
Muin Syed <mhc.msyed@gmail.com>
2020-12-20 21:15:15 -0500

    Added cors middleware to api route /api/hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
 }
